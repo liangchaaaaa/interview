292bde92bcff2bdaa870a078500a5d50dd5a2bbb
liangchaaaaa <git>
2025-12-22 23:50:38 +0800

    修正配置

--- src/main/java/com/example/demo/biz/controller/BizUserController.java
+++ src/main/java/com/example/demo/biz/controller/BizUserController.java
@@ -1,11 +1,3 @@
-/*
- * @Author: liangchaaaaa git
- * @Date: 2025-12-22 21:58:16
- * @LastEditors: liangchaaaaa git
- * @LastEditTime: 2025-12-22 23:15:25
- * @FilePath: \origin\src\main\java\com\example\demo\biz\controller\BizUserController.java
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
 package com.example.demo.biz.controller;
 
 import com.example.demo.api.dto.*;
